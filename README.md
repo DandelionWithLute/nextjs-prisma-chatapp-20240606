dc3f26b680f8d5d8ab3a2ab09566bdbb7fe40dcc
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-06-06 13:05:23 -0400

    main

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 chatapp
 create uuid room and store it into the database
+
+to be done later:
+1.password.js userInputToGet in bcrypt
--- utils/password.js
+++ utils/password.js
@@ -1,1 +1,23 @@
 // This file is for authjs
+// https://www.npmjs.com/package/bcrypt
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = "userInputToGet";
+
+// Technique 1 (generate a salt and hash on separate function calls):
+// bcrypt.genSalt(saltRounds, function(err, salt) {
+//     bcrypt.hash(myPlaintextPassword, salt, function(err, hash) {
+//         // Store hash in your password DB.
+//     });
+// });
+
+// Technique 2 (auto-gen a salt and hash):
+let hashedPassword;
+bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+  // Store hash in your password DB.
+  console.log(hash);
+  hashedPassword = hash;
+});
+
+console.log(hashedPassword);
+export default hashedPassword
