61f990740e65d8799be98dd5caac5ef39e3d4e7f
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-06-07 21:16:30 -0400

    main
    zod is ok

--- README.md
+++ README.md
@@ -7,6 +7,7 @@ errors met:
 to be done later:
 1.password.js userInputToGet in bcrypt
 2.add zod
+https://zod.dev/?id=schema-methods
 https://authjs.dev/getting-started/authentication/credentials
 3.I should delete ./utils/password.js and just leave it to the callback.
 4.Register page and Login page
--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,20 +1,40 @@
+// https://zod.dev/?id=schema-methods
 import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server";
-import bcrypt, { hash } from "bcrypt";
+import bcrypt from "bcrypt";
+import z from "zod";
 
 export const POST = async (req) => {
   // get info from frontend
   let body = await req.json();
-  const { name, email, passwordToHash } = body;
-  console.log(name, email, passwordToHash);
+  // const { name, email, passwordToHash } = body; // Discarded
+  // console.log(name, email, passwordToHash);
+
+  // zod schema
+  let nameSchema = z.string().refine((val) => val.length <= 20, {
+    message: "Length should be more than 20.",
+  });
+  let emailSchema = z
+    .string()
+    .email()
+    .refine((val) => val.length <= 99, {
+      message: "Length should be more than 20.",
+    });
+  let passwordSchema = z.string().refine((val) => val.length <= 99, {
+    message: "Length should be more than 20.",
+  });
+
+  let parsedName = nameSchema.parse(body.name);
+  let parsedEmail = emailSchema.parse(body.email);
+  let parsedPassword = passwordSchema.parse(body.passwordToHash);
 
   // hash password
-  let hashedPassword = await bcrypt.hash(passwordToHash, 10);
+  let hashedPassword = await bcrypt.hash(parsedPassword, 10);
 
   await prisma.user.create({
     data: {
-      name,
-      email,
+      name: parsedName,
+      email: parsedEmail,
       hashedPassword,
     },
   });
