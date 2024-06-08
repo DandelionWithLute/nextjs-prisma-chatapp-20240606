6464a3318f043f0ce8c8264da5d5021543ab3b49
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-06-08 04:30:51 -0400

    main
    before build

--- app/page.js
+++ app/page.js
@@ -1,16 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
-import { auth } from "@/auth";
+import { useSession } from "next-auth/react";
 
-const page = async () => {
-  const session = await auth();
-  console.log(session.user);
+const page = () => {
+  let [loaded, setLoaded] = useState();
+  // console.log(loading);
+  useEffect(() => {
+    funWithUseEffWillWork();
+  }, []);
+
+  function funWithUseEffWillWork() {
+    setLoaded(true);
+  }
+
+  const session = useSession();
+  if (loaded) {
+    console.log(session);
+  }
+
   return (
     <div className={styles.container}>
       <Link href={"/account"}>account</Link>
       <Link href={"/login"}>login</Link>
       <Link href={"/register"}>register</Link>
+      <div>
+        {loaded ? (
+          <div>useEffect Hook Successfully loaded the DOM!</div>
+        ) : (
+          <div>Still loading...</div>
+        )}
+      </div>
     </div>
   );
 };
