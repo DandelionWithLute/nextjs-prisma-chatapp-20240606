01cc71f00bfa96d382b3ea68e2280efdf118ec42
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2024-06-07 22:43:06 -0400

    Update auth.js
    successfully finished authjs login and register

--- auth.js
+++ auth.js
@@ -1,7 +1,10 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 // Your own logic for dealing with plaintext password strings; be careful!
+// import { saltAndHashPassword } from "@/utils/password"
+import prisma from "./utils/prisma";
 import bcrypt from "bcrypt";
+import { NextResponse } from "next/server";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
@@ -13,19 +16,33 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         password: {},
       },
       authorize: async (credentials) => {
-        let user = null;
+        // let user = null
 
         // logic to salt and hash password
-        const pwHash = bcrypt.hash(credentials.password, 25);
-        // saltAndHashPassword(credentials.password);
+        // const pwHash = saltAndHashPassword(credentials.password)
 
         // logic to verify if user exists
-        user = await getUserFromDb(credentials.email, pwHash);
+        // user = await getUserFromDb(credentials.email, pwHash)
 
-        if (!user) {
-          // No user found, so this is their first attempt to login
-          // meaning this is also the place you could do registration
-          throw new Error("User not found.");
+        let userToCheck = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!userToCheck) {
+          return new NextResponse(
+            JSON.stringify("Account or Password wrong.", { status: 200 })
+          );
+        }
+        let isPasswordCorrect = await bcrypt.compare(
+          credentials.password,
+          userToCheck.hashedPassword
+        );
+
+        if (!isPasswordCorrect) {
+          return new NextResponse(
+            JSON.stringify("Account or Password wrong.", { status: 200 })
+          );
         }
 
         // return user object with the their profile data
